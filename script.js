7cfd93de5fddf76f4962757120770da77decee40
armharutunyan <hashasrj@gmail.com>
2023-05-29 20:41:08 +0400

    p

---
+++ script.js
@@ -0,0 +1,158 @@
+// // Object chunen erkarutyun, zangvac [0] chenq karox tal nranc;
+// // objecti vra cikl chenq karox bacel qani vor erkarutyun, length chuni;
+// // objectnerum pahvac amen ban shat teteven ashxatum canrutyun chen talis mer codin,
+// // { , } => Nmane Css faylin; 
+// // unni ir sepakan cikly,
+// // hnaravorutyun drsic jnjelu,
+// // ev drsic avelacnel, kam popoxelu
+// // bajanvume 2 tesaki erkar                     => Object.create({},{});
+// // ev karch vori vra mi sharq functionner bacen => true; => {,};
+
+// // delete => Te vor arjeqy; Jnjelu hstak hraman;
+
+// // key, banali => Dzax koxmi grvacqnery;
+// // value, Arjeqner => Aj koxmi grvacqnery;
+
+// // innerHTML   => HTML ayin detali mej avelacnel grvacq + HTML ayin teg;
+// // innerText   => HTML ayin detali mej avelacnel grvacq;
+// // textContent => HTML ayin detali mej avelacnel grvacq;
+
+
+// // 1 mardkayin tvyal;
+// // let obj = {
+// //     name: 'Taguhi',
+// //     surname: 'Araqelyan',
+// //     age: 22,
+// //     budget: 450000,
+// // }
+
+// // h1.style.background = 'orangered'; => Css;
+
+// // innerText;
+// // h1.innerText = `${obj.name}`;
+// // h2.innerText = `${obj.surname}`;
+// // h3.innerText = `${obj.age}`;
+// // h4.innerText = `${obj.budget}`;
+
+// // textContent;
+// // h1.textContent = `${obj.name}`;
+// // h2.textContent = `${obj.surname}`;
+// // h3.textContent = `${obj.age}`;
+// // h4.textContent = `${obj.budget}`;
+
+
+// // innerHTML;
+// // h1.innerHTML = `${obj.name}`;
+// // h2.innerHTML = `${obj.surname}`;
+// // h3.innerHTML = `${obj.age}`;
+// // h4.innerHTML = `${obj.budget}`;
+
+// // button.textContent = `${obj.name}`;
+
+
+// let obj = {
+//     t: {
+//         name: 'Taguhi',
+//         surname: 'Araqelyan',
+//         age: 22,
+//         budget: 450000
+//     },
+// }
+// obj.t.xy = 'x';
+
+// // clear.onclcik = function(){
+// //     delete obj.t.xy;
+// // delete obj.t.budget;
+// // delete obj.t.age;
+// // }
+
+// console.log(obj.t)
+// // aButton.onclick = function () {
+// //     obj.a.name = 'Ani';
+// //     obj.a.surname = 'Khahramanyan';
+// //     obj.a.age = 19;
+// //     obj.a.budget = 450000;
+
+// // }
+
+
+// // h1.textContent = obj.a.name;
+// // h2.textContent = obj.b.name;
+
+
+
+
+// let obj = {
+//     a: {
+//         name: 'Taguhi',
+//         surname: 'Araqelyan',
+//         age: 22,
+//         budget: 450000
+//     },
+//     b: {
+//         name: 'Tamara',
+//         surname: 'Avetisyan',
+//         age: 23,
+//         budget: 500000
+//     },
+//     c: {
+//         name: 'Tatev',
+//         surname: 'Aroyan',
+//         age: 24,
+//         budget: 550000
+//     },
+//     d: {
+//         name: 'Toma',
+//         surname: 'Kharabaxcyan',
+//         age: 25,
+//         budget: 600000
+//     },
+//     e: {
+//         name: 'Tina',
+//         surname: 'Kaloyan',
+//         age: 26,
+//         budget: 650000
+//     },
+// }
+
+
+
+
+// // Yes greluem console.log(obj) => Tox beri txaneri tvyalner + inch avto unen;
+
+
+
+
+let obj = {
+    a: {
+        name: 'Temur',
+        surname: 'Snjoyan',
+        age: 22,
+        budget: 450000
+    },
+    b: {
+        name: 'Tata',
+        surname: 'Simonyan',
+        age: 23,
+        budget: 500000
+    },
+    c: {
+        name: 'Tyom',
+        surname: 'Aroyan',
+        age: 24,
+        budget: 550000
+    },
+    d: {
+        name: 'Taron',
+        surname: 'Kharabaxcyan',
+        age: 25,
+        budget: 600000
+    },
+    e: {
+        name: 'Tigran',
+        surname: 'Kaloyan',
+        age: 26,
+        budget: 650000
+    },
+}
+// Yes greluem console.log(obj) => Tox beri Axjikneri tvyalner + inch avto unen;
